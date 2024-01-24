b8e080a9cbb2d56f5a93d823e5b161e81647e698
VaibhavShinde3437 <155080750+VaibhavShinde3437@users.noreply.github.com>
2024-01-24 18:08:05 +0530

    Revert "updates in code"

--- authentication/models.py
+++ authentication/models.py
@@ -49,9 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Assessment(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
-    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='assessments_created')
-    update_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='assessments_updated')
+    description = models.TextField()
 
         
     def __str__(self):
@@ -65,13 +63,11 @@ class Question(models.Model):
         ('rating', 'Rating'),
     ]
 
-    assessment = models.ForeignKey(to=Assessment, on_delete=models.CASCADE, default=0)
+    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     options = models.JSONField(default=list)
     question_type = models.CharField(max_length=20, choices=CHOICES)
-    description = models.TextField(blank=True, null=True)
-    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='questions_created')
-    update_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='questions_updated')
+    description = models.TextField()
 
     def __str__(self):
         return self.title
--- authentication/urls.py
+++ authentication/urls.py
@@ -4,17 +4,15 @@ from .views import AssessmentView, QuestionView, RegisterView, LoginView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'assessments', AssessmentView, basename='assessment')
+router.register(r'assessment', AssessmentView, basename='assessment')
 
-router.register(r'questions', QuestionView, basename='question')
+router.register(r'question', QuestionView, basename='question')
 
-router2 = DefaultRouter()
-router2.register(r'questions', QuestionView, basename='que')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('api/', include(router.urls)),
-    path('api/', include(router2.urls)),
+    # path('api/assessment/', include(router.urls)),
 
 ]
